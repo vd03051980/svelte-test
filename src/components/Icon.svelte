5cd2a570991f55222c243605062d62053a2e1e92
Kristina <yurteeva.Kristinamengli@bk.ru>
2024-03-11 00:44:41 +0500

    add todo app

---
+++ src/components/Icon.svelte
@@ -0,0 +1,14 @@
+<script>
+export let name;
+const icons = [
+{
+name: 'check-mark',
+path: '<path d="M19.77 4.9311.4 1.4L8.43 19.071-5.6-5.6 1.4-1.4 4.2 4.2L19.77 4.93m0-2.83L8.43 13.441-4.2-4.2L0 13.4718.43 8.43L24 6.33 19.77 2.1z" fill="currentColor"></path>'},
+
+{
+name: 'delete',
+path: '<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-51-1-1h-51-1 1H5v2h14V4h-3.5z" fill="currentColor"></path>',
+},];
+let icon = icons.find((item) => item.name === name); </script>
+
+
