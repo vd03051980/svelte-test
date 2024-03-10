de984a3ae0c1d8912b3cc3506a4d275dcc6c9fad
Kristina <yurteeva.Kristinamengli@bk.ru>
2024-03-10 23:20:48 +0500

    add blog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,11 @@
         <a href="/" class="nav-logo" title="Back to Homepage">Фамилия Имя</a>
         <div class="nav-links">
            {#each nav as link}
-           <a href={link.href} class="link">{link.title}</a>
+           {#if link.isShow}
+
+<a href={link.href} class="link">{link.title}</a>
+
+{/if}
            {/each}
         </div>
     </div>
@@ -15,23 +19,28 @@
     const nav = [
         {
             title: 'Обо мне',
-            href: '/about'
+            href: '/about',
+            isShow: true,
         },
         {
             title: 'Блог',
-            href: '/blog'
+            href: '/blog',
+            isShow: true,
         },
         {
             title: 'Портфолио',
-            href: '/project'
+            href: '/project',
+            isShow: true,
         },
         {
             title: 'Контакты',
-            href: '/contact'
+            href: '/contact',
+            isShow: true,
         },
         {
             title: 'temp',
-            href: '/temp'
+            href: '/temp',
+           
         },
     ]
 </script>
