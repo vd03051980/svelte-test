04e7e498263505ef42adff0f4406d605886e7273
Kristina <yurteeva.Kristinamengli@bk.ru>
2024-03-10 21:37:01 +0500

    add main page and nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<nav>
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to Homepage">Фамилия Имя</a>
+        <div class="nav-links">
+            <a href="/about" class="link">Обо мне</a>
+            <a href="/blog" class="link">Блог</a>
+            <a href="/projects" class="link">Портфолио</a>
+            <a href="/contact" class="link">Контакты</a>
+        </div>
+    </div>
+</nav>
+<div class="container">
+    <slot></slot>
+</div>
+<style>
+    .container{
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,80 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+<title>Имя Фамилия</title>
+</svelte:head>
+<div class='container'>
+    <div class='info'>
+        <div class='animate-background'>
+            <div class='container info-container'>
+                <div class='info-text'>
+<h1>
+<p class='animate-text-1'>Lorem ipsum dolor sit,</p>
+<p class='text-2'>Lorem ipsum dolor sit amet.</p>
+</h1>
+</div>
+<h2 class='subtitle'>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto aut,
+cupiditate dolores
+ducimus expedita labore nemo odio quibusdam quidem sapiente soluta.
+</h2>
+<div style='color:black' class='scroll-button animate-scroll' tabindex='0'>
+<a href='/about'>Click Here</a>
+<div class='arrow'>➜</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+   
+<div class='background-div'></div>
+<style>
+    .info{
+        text-align: center;
+        position: relative;
+    }
+    .info-container{
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: left;
+        align-items: start;
+    }
+    .container{
+        max-width: 1400px;
+        margin: 0;
+    }
+    .info h1 p{
+        position: relative;
+        font: inherit;
+        margin: 0;
+        text-align: left;
+        list-style: 130%;
+        width: fit-content;
+        font-weight: 800;
+        letter-spacing: -0.03em;
+    }
+    .info h2{
+        max-width: 65ch;
+        font-weight: 200;
+        margin-top: 0.50rem;
+    }
+    .background-div{
+        background-color: #ecebe8;
+        width: 100%;
+        height: 56vh;
+        margin: -205px auto;
+    }
+    .info .info-text{
+        position: relative;
+    }
+    .scroll-button{
+        display: flex;
+        border: none;
+        outline: none;
+        cursor: pointer;
+    }
+
+    .scroll-button .arrow{
+        margin-left: 0.50rem;
+    }
+</style>
